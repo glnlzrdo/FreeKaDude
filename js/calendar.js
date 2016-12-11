49a8f17113be1becdeca61ada911e823fdd0004e
Glenn Lizardo <gdlizardo@up.edu.ph>
2016-12-11 19:13:20 +0800

    Completed "Add, Edit, Delete Activity" features

--- js/calendar.js
+++ js/calendar.js
@@ -6,12 +6,12 @@ $(document).ready(function () {
     var YESTERDAY = todayDate.clone().subtract(1, 'day').format('YYYY-MM-DD');
     var TODAY = todayDate.format('YYYY-MM-DD');
     var TOMORROW = todayDate.clone().add(1, 'day').format('YYYY-MM-DD');
+    var dstart, dend, currentEvent;
     // page is now ready, initialize the calendar...
 
     var calendar = $('#calendar').fullCalendar({
-        // put your options and callbacks here
-        theme: Boolean,
-        default: true,
+        theme: true,
+        //default: true,
         header: {
             left: 'month,agendaWeek,agendaDay,list',
             center: 'title'
@@ -31,155 +31,97 @@ $(document).ready(function () {
         }, {
             title: 'Project Deadline',
             start: YM + '-14',
-            end: YM + '-14'
+            end: YM + '-15'
         }],
-        eventMouseover: function(event, jsEvent, view) {
+        eventMouseover: function (event, jsEvent, view) {
             if (view.name !== 'agendaDay') {
                 $(jsEvent.target).attr('title', event.title);
             }
         },
-        /*
+
         eventClick: function (calEvent, jsEvent, view) {
+            // var r = confirm("Delete " + calEvent.title + "?");
+            // if (r === true) {
+            //     $('#calendar').fullCalendar('removeEvents', calEvent._id);
+            // }
 
-            alert('Event: ' + calEvent.title);
-            alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
-            alert('View: ' + view.name);
+            $('#inputActivity').val(calEvent.title);
+            var starttime = moment(calEvent.start).format('dddd MMM DD, h:mm a');
+            var endtime = moment(calEvent._end).format('dddd MMM DD, h:mm a');
+            var mywhen = starttime + ' - ' + endtime;
+            $('#createEventModal #editStartTime').val(moment(calEvent.start));
+            $('#createEventModal #editEndTime').val(moment(calEvent._end));
 
-            // change the border color just for fun
-            $(this).css('border-color', 'red');
+            $('#editEventModal #editWhen').text(mywhen);
+            $('#editEventModal').modal('show');
+            dstart = calEvent.start;
+            dend = calEvent._end;
+            currentEvent = calEvent._id;
 
-        },
-        */
-        
-        eventClick: function(calEvent, jsEvent, view)
-        {
-            var r=confirm("Delete " + calEvent.title + "?");
-            if (r===true)
-              {
-                  $('#calendar').fullCalendar('removeEvents', calEvent._id);
-              }
         },
 
         selectHelper: true,
 
-        /*
-
         select: function (start, end) {
-
+            $('#activityName').val("");
             var starttime = moment(start).format('dddd MMM DD, h:mm a');
             var endtime = moment(end).format('h:mm a');
             var mywhen = starttime + ' - ' + endtime;
             $('#createEventModal #apptStartTime').val(moment(start));
             $('#createEventModal #apptEndTime').val(moment(end));
-            
-            //$('#createEventModal #apptAllDay').val(allDay);
+
             $('#createEventModal #when').text(mywhen);
             $('#createEventModal').modal('show');
-
-            $('#prompt-save').on('click', function (e) {
-                // We don't want this to act as a link so cancel the link action
-                e.preventDefault();
-
-                doSubmit();
-            });
-
-            function doSubmit() {
-                $("#createEventModal").modal('hide');
-                
-                console.log($('#apptStartTime').val());
-                console.log($('#apptEndTime').val());
-                console.log($('#apptAllDay').val());
-                //alert("form submitted");
-
-                  
-                // $("#calendar").fullCalendar('renderEvent',
-                //     {
-                //         title: $('#activityName').val(),
-                //         start: new Date($('#apptStartTime').val()),
-                //         end: new Date($('#apptEndTime').val()),
-                //         //allDay: ($('#apptAllDay').val() == "true"),
-                //     },
-                //     true);
-                
-                
-                var title = $.trim($('#activityName').val());
-                var eventData;
-				if (title) {
-					eventData = {
-						title: title,
-						start: start,
-						end: end
-					};
-					$('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
-				}
-				$('#calendar').fullCalendar('unselect');
-            }
-
+            dstart = start;
+            dend = end;
         }
 
-        */
-
-        
-        select: function(start, end) {
-            var title = prompt('Activity Description:');
-            var eventData;
-            if (title) {
-                eventData = {
-                    title: title,
-                    start: start,
-                    end: end
-                };
-                $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
-            }
-            $('#calendar').fullCalendar('unselect');
-        }
-        
 
     });
-
-
-    /*
-
-    function selectCal(start, end, allDay) {
-        //var endtime = $.fullCalendar.formatDate(end, 'h:mm tt');
-        //var starttime = $.fullCalendar.formatDate(start, 'ddd, MMM d, h:mm tt');
-        var starttime = moment(start).format('dddd MMM d, h:mm a');
-        var endtime = moment(end).format('h:mm a');
-        var mywhen = starttime + ' - ' + endtime;
-        $('#createEventModal #apptStartTime').val(start);
-        $('#createEventModal #apptEndTime').val(end);
-        $('#createEventModal #apptAllDay').val(allDay);
-        $('#createEventModal #when').text(mywhen);
-        $('#createEventModal').modal('show');
-
-    }
 
     $('#prompt-save').on('click', function (e) {
         // We don't want this to act as a link so cancel the link action
         e.preventDefault();
-
-        doSubmit();
+        $("#createEventModal").modal('hide');
+        var title = $('#activityName').val();
+        var eventData;
+        if (title) {
+            eventData = {
+                title: title,
+                start: dstart,
+                end: dend
+            };
+            $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+        }
+        $('#calendar').fullCalendar('unselect');
     });
 
-    function doSubmit() {
-        $("#createEventModal").modal('hide');
-        
-        console.log($('#apptStartTime').val());
-        console.log($('#apptEndTime').val());
-        console.log($('#apptAllDay').val());
-        alert("form submitted");
-          
-        $("#calendar").fullCalendar('renderEvent',
-            {
-                title: $('#patientName').val(),
-                start: new Date($('#apptStartTime').val()),
-                end: new Date($('#apptEndTime').val()),
-                allDay: ($('#apptAllDay').val() == "true"),
-            },
-            true);
-    }
+    $('#prompt-update').on('click', function (e) {
+        e.preventDefault();
+        $("#editEventModal").modal('hide');
+        var title = $('#inputActivity').val();
+        var eventData;
+        if (title) {
+            eventData = {
+                title: title,
+                start: dstart,
+                end: dend
+            };
+            $('#calendar').fullCalendar('removeEvents', currentEvent);
+            $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+        }
+        $('#calendar').fullCalendar('unselect');
+    });
 
-    */
+    $('#prompt-delete').on('click', function (e) {
+        e.preventDefault();
+         var r = confirm("Are you sure? This cannot be undone.");
+             if (r === true) {
+                 $('#calendar').fullCalendar('removeEvents', currentEvent);
+                 $("#editEventModal").modal('hide');
+             }
+        $('#calendar').fullCalendar('unselect');
+    });
 
 });
 
